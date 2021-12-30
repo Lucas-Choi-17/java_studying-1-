2af7b97aa17cd7bf3d491880b26110b2350a3f03
Choi Seung In <csissis@naver.com>
2021-12-30 17:33:42 +0900

    Console : window that shows result of communication between user and OS
    System.out.println("xx") : change the line after printing xx
    
    /n : new line
    /t : tab
    
    Data type
    -true & false : boolean (1byte)
    -whole number : int (4byte)
    -fractional number : double (8byte)
    -single character : char (2byte)
    -sequence of characters : string (according to string length)
    
    Conversion specifier
    %d : decimal
    %o : octal decimal
    %x : hexadecimal
    %f : fractional number
    %c : character
    %s : string

--- src/java_studying_1/PrintTest.java
+++ src/java_studying_1/PrintTest.java
@@ -3,6 +3,29 @@ package java_studying_1;
 public class PrintTest {
     public static void main(String[] args) {
 
-        System.out.print("Hello World!...");
+        System.out.print("Hello World!...\n");
+        System.out.println("man");
+
+        int i = 10;
+        long l = 100;
+        double d = 1.23;
+        float f = 1.23F;
+        char c = 'A';
+        String s = "Lucas";
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(d);
+        System.out.println(f);
+        System.out.println(c);
+        System.out.println(s);
+        System.out.println("-------------------");
+        System.out.printf("%d\n", i);
+        System.out.printf("%o\n", i);
+        System.out.printf("%x\n", i);
+        System.out.printf("%d\n", l);
+        System.out.printf("%f\n", d);
+        System.out.printf("%.2f\n", f);
+        System.out.printf("%c\n", c);
+        System.out.printf("%s\n", s);
     }
 }
